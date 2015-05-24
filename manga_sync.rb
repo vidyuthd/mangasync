4a443fd001293f3ec7cd5a6c2be5ea0cc13bb4e6
vidyuthd <vidyuth.bitsgoa@gmail.com>
2015-05-25 01:02:21 +0530

    Making it parallelized to download multiple chapters at the same time (3 threads currently)

--- manga_sync.rb
+++ manga_sync.rb
@@ -3,14 +3,12 @@ require 'open-uri'
 HTTP_URL_SEPERATOR = "/"
 
 class MangaSync
-  def connect_to_method
-    printf "input series name : "
-    series_name = gets().chomp().downcase
-    printf "input start chapter no : "
-    start_chapter = gets().chomp.downcase
+
+  def method(series_name, start_chapter)
+    local_start_chapter = start_chapter.clone
     page=1
     series_dir = File.join(File.absolute_path(''), (series_name.to_s))
-    chapter_dir = File.join(File.absolute_path(''), (series_name.to_s), (start_chapter.to_s))
+    chapter_dir = File.join(File.absolute_path(''), (series_name.to_s), (local_start_chapter.to_s))
     if (!Dir.exist?(series_dir))
       Dir.mkdir(series_dir, 0755)
     end
@@ -20,8 +18,8 @@ class MangaSync
     while File.exist?(File.join(chapter_dir.to_s, page.to_s+".png"))
       page=page+1
     end
-    while open(("http://www.mangareader.net/"+series_name+HTTP_URL_SEPERATOR+start_chapter+HTTP_URL_SEPERATOR+page.to_s)) { |f|
-      p "hitting url:  "+"http://www.mangareader.net/"+series_name+HTTP_URL_SEPERATOR+start_chapter.to_s+HTTP_URL_SEPERATOR+page.to_s
+    while open(("http://www.mangareader.net/"+series_name+HTTP_URL_SEPERATOR+local_start_chapter+HTTP_URL_SEPERATOR+page.to_s)) { |f|
+      p "hitting url:  "+"http://www.mangareader.net/"+series_name+HTTP_URL_SEPERATOR+local_start_chapter.to_s+HTTP_URL_SEPERATOR+page.to_s
       f.each_line { |line|
         if url = line.to_s.match(/"http.*jpg"/).to_s.match(/http.*jpg/)
           file_to_write = File.join(chapter_dir.to_s, page.to_s+".png")
@@ -38,6 +36,29 @@ class MangaSync
     end
   rescue OpenURI::HTTPError
     puts $!, $@
+  end
+
+  def connect_to_method
+    printf "input series name : "
+    series_name = gets().chomp().downcase
+    printf "input start chapter no : "
+    start_chapter = gets().chomp.downcase
+    printf "input end chapter no : "
+    end_chapter = gets().chomp.downcase
+    start = start_chapter
+
+    while start_chapter.to_i <= end_chapter.to_i
+      if start_chapter==start
+        t1 = Thread.new(series_name, start_chapter) { method(series_name, start_chapter) }
+      else
+        t1 = Thread.new(series_name, start_chapter) { method(series_name, (start_chapter.next!)) }
+      end
+      t2 = Thread.new(series_name, start_chapter) { method(series_name, (start_chapter.next!)) }
+      t3 = Thread.new(series_name, start_chapter) { method(series_name, (start_chapter.next!)) }
+      t1.join
+      t2.join
+      t3.join
+    end
   end
 end
 
